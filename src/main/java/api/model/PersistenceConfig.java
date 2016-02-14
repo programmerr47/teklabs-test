ee9f05b8e4ea0995644e777ea6cc864057e276a8
george <egorichlev@yandex.ru>
2016-02-14 21:27:48 +0300

    add liquibase configuration create user table

---
+++ src/main/java/api/model/PersistenceConfig.java
@@ -0,0 +1,30 @@
+package api.model;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class PersistenceConfig {
+
+    @Bean
+    public SpringLiquibase springLiquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource());
+        liquibase.setChangeLog("classpath:changelog/db-changelog.xml");
+        return liquibase;
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
