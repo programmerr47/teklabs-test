8d19f0b5567ccf7da6c023fa840ea2bb46ebe9e8
george <egorichlev@yandex.ru>
2016-02-14 19:59:28 +0300

    add user model

---
+++ src/main/java/api/model/User.java
@@ -0,0 +1,19 @@
+package api.model;
+
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
